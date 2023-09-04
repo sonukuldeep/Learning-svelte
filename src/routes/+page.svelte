e776e04bfd7cf8fa171c6555b07ab4b3af30c0c7
sonukuldeep <sonukuldeep@outlook.com>
2023-09-04 10:35:44 +0530

    introduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let name = '<s>Svelte</s>';
+	let src = `https://api.dicebear.com/7.x/pixel-art/svg?seed=${name.toLowerCase()}`;
+	import Nested from './Nested.svelte';
+</script>
+
+<h1>Hello, {name}</h1>
+<p>Hello, {@html name}</p>
+
+<img {src} alt="A dancing man" />
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia asperiores nobis veniam dolores</p>
+
+<Nested />
+
+<style>
+	img {
+		width: 200px;
+	}
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
---
+++ src/routes/Nested.svelte
@@ -0,0 +1,4 @@
+<script>
+</script>
+
+<p>Hello this is another paragraph</p>
