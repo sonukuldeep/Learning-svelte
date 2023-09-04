1e7a1a0ee67c23ad8c7d93bba044dad08a29424b
sonukuldeep <sonukuldeep@outlook.com>
2023-09-04 13:15:26 +0530

    logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,67 @@
 <script>
-	import Nested from './Nested.svelte';
 	import PackageInfo from './PackageInfo.svelte';
-	let packageInfo = {
-		name: 'Json',
-		version: '0.1.1',
-		speed: '2',
-		website: 'jsonbond.com'
-	};
+	import Thing from './Thing.svelte';
+
+	let count = 0;
+	let colours = new Set(['red', 'green', 'blue', 'yellow', 'violet', 'pink']);
+	function increment() {
+		count += 1;
+	}
+
+	let things = [
+		{ id: 1, name: 'apple' },
+		{ id: 2, name: 'banana' },
+		{ id: 3, name: 'carrot' },
+		{ id: 4, name: 'doughnut' },
+		{ id: 5, name: 'egg' }
+	];
+
+	function popThing() {
+		things = things.slice(1);
+	}
+
+	let promised = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(1);
+		}, 2000);
+		setTimeout(() => {
+			reject(0);
+		}, 1000);
+	});
 </script>
 
-<Nested answer={'42'} />
-<Nested />
+<p>Modify count</p>
 
-<PackageInfo {...packageInfo} />
+<button on:click={increment}>Clicked {count}</button>
+
+{#if count > 10}
+	<p>This appears once count is &gt; 10</p>
+{:else if count > 5}
+	<p>Current value of count is {count}</p>
+{:else}
+	<p>Currnet value less than 5</p>
+{/if}
+
+{#each colours as colour, index}
+	<span style="background-color: {colour};padding: 5px;margin: 2px"
+		>{colour} index position {index}</span
+	>
+{/each}
+
+<button on:click={popThing}>Pop</button>
+{#each things as item (item.id)}
+	// key is very important
+	<Thing name={item.name} />
+{/each}
+
+{#await promised}
+	<p>loading...</p>
+{:then val}
+	<p>The return value is {val}</p>
+{:catch error}
+	<p>Error error code: {error}</p>
+{/await}
+
+<!-- {#await promised then val}
+	<p>The return value is {val}</p>
+{/await} -->
