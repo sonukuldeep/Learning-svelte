77226b25e7b7081df18106afb57c991a81e1b080
sonukuldeep <sonukuldeep@outlook.com>
2023-09-04 22:36:38 +0530

    binding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,55 @@
 <script>
-	import Outer from './Outer.svelte';
-	let m = { x: 0, y: 0 };
+	let name = 'world';
+	let num1 = 0;
+	let numRange = 5;
+	let checked = false;
 
-	function handleMove(e) {
-		m.x = e.clientX;
-		m.y = e.clientY;
-	}
+	let selected;
 
-	function handleMessage(e) {
-		console.log(e.detail);
-	}
+	let scoops = 1;
 
-	function handleClick() {
-		console.log('big btn clicked');
-	}
+	let uniform = '';
+	let area;
 </script>
 
-<div on:mousemove|once={handleMove}>
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum accusantium odio, maiores eum,
-	similique, deleniti vel animi dolorum minima illo omnis doloribus aspernatur quibusdam ipsa
-	perspiciatis. Necessitatibus, qui laudantium cumque reprehenderit corrupti totam consequatur nam
-	similique ducimus veniam maxime tempora culpa illum quas est doloribus placeat possimus libero
-	nemo sequi, numquam molestiae provident temporibus nisi. Quaerat, neque facilis! Ad perspiciatis
-	hic dicta aliquam est provident placeat qui quae cupiditate perferendis, ipsa dolorem, officiis
-	deleniti aliquid minima nostrum nemo. Architecto exercitationem temporibus facere error, ex quam?
-	Maxime, dolor eveniet incidunt soluta corrupti ratione quae, dolore modi consequuntur corporis
-	perspiciatis ad? Laborum.
-</div>
+<input type="text" bind:value={name} />
+<p>Hello, {name}</p>
 
-<p>The pointer is at {m.x} x {m.y}</p>
+<input type="number" bind:value={num1} />
 
-<Outer on:msg={handleMessage} on:click={handleClick} />
+<p>number is {num1} and its type is {typeof num1}</p>
+
+<input type="range" min="0" max="10" bind:value={numRange} />
+
+<p>range is {numRange}</p>
+
+<input type="checkbox" bind:checked />
+
+<p>Checked {checked}</p>
+
+<select bind:value={selected}>
+	<option value="car">Car</option>
+	<option value="truck">Truck</option>
+	<option value="bike">Bike</option>
+</select>
+
+<p>Selected vehicle {selected}</p>
+
+{#each [1, 2, 3] as item}
+	<input type="radio" name="scoops" value={item} bind:group={scoops} />
+{/each}
+
+<p>Scoop is {scoops}</p>
+
+<select multiple bind:value={uniform}>
+	{#each ['shoe', 'shirt', 'tie', 'bag'] as item}
+		<option value={item}>{item}</option>
+	{/each}
+</select>
+
+<p>Multi select {uniform}</p>
+
+<br />
+<textarea bind:value={area} cols="30" rows="10" />
+
+<p>{area}</p>
