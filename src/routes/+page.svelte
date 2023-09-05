809bc5ab32c0e52329aeb798dd55a939c0293468
sonukuldeep <sonukuldeep@outlook.com>
2023-09-05 21:01:45 +0530

    transitions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,73 @@
 <script>
-	import { tweened } from 'svelte/motion';
-	import { cubicOut } from 'svelte/easing';
-	import Spring from './Spring.svelte';
+	import { fade, fly } from 'svelte/transition';
+	import { elasticOut } from 'svelte/easing';
+	import TypewritingEffect from './TypewritingEffect.svelte';
+	import GlobalTransition from './GlobalTransition.svelte';
+	function spin(_, { duration }) {
+		return {
+			duration,
+			css: (t) => {
+				const eased = elasticOut(t);
+				return `
+				transform: scale(${eased}) rotate(${eased * 1000}deg);
+				color: hsl(
+					${Math.trunc(t * 360)},
+					${Math.min(100, 1000 * (1 - t))}%,
+					${Math.min(50, 500 * (1 - t))}%
+				)
+				`;
+			}
+		};
+	}
+	let fadeTransition = true;
+	let flyTransition = true;
+	let inOut = false;
 
-	const progress = tweened(0, { easing: cubicOut, duration: 400 });
+	let customTransition = false;
 </script>
 
-<progress value={$progress} />
-<br />
-<button on:click={() => progress.set(0)}>0%</button>
-<button on:click={() => progress.set(0.25)}>25%</button>
-<button on:click={() => progress.set(0.5)}>50%</button>
-<button on:click={() => progress.set(0.75)}>75%</button>
-<button on:click={() => progress.set(1)}>100%</button>
+<input type="checkbox" bind:checked={fadeTransition} />
 
-<Spring />
+{#if fadeTransition}
+	<p transition:fade>Toggle to see transition</p>
+{/if}
+
+<input type="checkbox" bind:checked={flyTransition} />
+
+{#if flyTransition}
+	<p transition:fly={{ y: 200, duration: 2000 }}>Toggle to see transition</p>
+{/if}
+
+<input type="checkbox" bind:checked={inOut} />
+
+{#if inOut}
+	<p in:fade out:fly={{ y: 200, duration: 2000 }}>Toggle to see transition</p>
+{/if}
+
+<br />
+<input type="checkbox" bind:checked={customTransition} />
+
+{#if customTransition}
+	<div class="centered" in:spin={{ duration: 8000 }} out:fade>
+		<span>transitions!</span>
+	</div>
+{/if}
+<br />
+<TypewritingEffect />
+<br />
+<GlobalTransition />
 
 <style>
-	progress {
-		width: 600px;
+	.centered {
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	span {
+		position: absolute;
+		transform: translate(-50%, -50%);
+		font-size: 4em;
 	}
 </style>
---
+++ src/routes/GlobalTransition.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { fade } from 'svelte/transition';
+
+	let showItems = true;
+	let i = 5;
+	let items = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+</script>
+
+<label>
+	<input type="checkbox" bind:checked={showItems} />
+	show list
+</label>
+<br />
+<label>
+	<input type="range" bind:value={i} max="10" />
+</label>
+
+{#if showItems}
+	{#each items.slice(0, i) as item}
+		<div transition:fade|global>
+			{item}
+		</div>
+	{/each}
+{/if}
+
+<style>
+	div {
+		padding: 0.5em 0;
+		border-top: 1px solid #eee;
+	}
+</style>
